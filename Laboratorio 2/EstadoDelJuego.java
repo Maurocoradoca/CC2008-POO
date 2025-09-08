50cdf4389dfb06617d1f04515e6ac09611f3c6d9
Maurocoradoca <mauricioadriancoradoca@gmail.com>
2025-09-08 13:50:52 -0600

    Clase EstadoDelJuego y Puntaje

--- Laboratorio 2/EstadoDelJuego.java
+++ Laboratorio 2/EstadoDelJuego.java
@@ -1,3 +1,40 @@
 public class EstadoDelJuego {
-    
+    private int jugadorActual;   
+    private int totalPares;      
+    private int encontrados;    
+
+    public EstadoDelJuego(int totalPares) {
+        this.totalPares = totalPares;
+        this.encontrados = 0;
+        this.jugadorActual = 0; 
+    }
+
+    public int actual() {
+        return jugadorActual;
+    }
+
+    public void mantenerTurno() {
+    }
+
+    public void cambiarTurno() {
+        jugadorActual = (jugadorActual == 0) ? 1 : 0;
+    }
+
+    public void incEncontrados() {
+        encontrados++;
+    }
+
+    public boolean terminado() {
+        return encontrados >= totalPares;
+    }
+
+    public void reiniciar(int totalPares) {
+        this.totalPares = totalPares;
+        this.encontrados = 0;
+        this.jugadorActual = 0;
+    }
+
+    // Getters 
+    public int getTotalPares() { return totalPares; }
+    public int getEncontrados() { return encontrados; }
 }
--- Laboratorio 2/Puntaje.java
+++ Laboratorio 2/Puntaje.java
@@ -1,3 +1,27 @@
 public class Puntaje {
-    
+    private int p1Pares;
+    private int p2Pares;
+    private int ganador;
+
+    public Puntaje(int p1Pares, int p2Pares, int ganador) {
+        this.p1Pares = p1Pares;
+        this.p2Pares = p2Pares;
+        this.ganador = ganador;
+    }
+
+    public int ganador() { return ganador; }
+
+    public String resumen(String nombre1, String nombre2) {
+        if (ganador == -1) {
+            return "Empate " + nombre1 + " " + p1Pares + " - " + p2Pares + " " + nombre2;
+        } else if (ganador == 0) {
+            return "Ganó " + nombre1 + " (" + p1Pares + " a " + p2Pares + ")";
+        } else {
+            return "Ganó " + nombre2 + " (" + p2Pares + " a " + p1Pares + ")";
+        }
+    }
+
+    public int getP1Pares() { return p1Pares; }
+    public int getP2Pares() { return p2Pares; }
+    public int getGanador() { return ganador; }
 }
