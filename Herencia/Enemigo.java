5ca66a2c69a512ebd6a8ae03b5fef7119a653f77
Maurocoradoca <mauricioadriancoradoca@gmail.com>
2025-09-28 11:45:45 -0600

    Enemigos y sus tipos

--- Herencia/Enemigo.java
+++ Herencia/Enemigo.java
@@ -1,3 +1,86 @@
-public class Enemigo {
+import java.util.Random;
+
+public abstract class Enemigo extends Combatiente {
+    protected TipoEnemigo tipo;
+    protected boolean esJefe;
+    protected Random random;
     
+    public Enemigo(String nombre, TipoEnemigo tipo, int hp, int atk, boolean esJefe) {
+        super(nombre, hp, atk);
+        this.tipo = tipo;
+        this.esJefe = esJefe;
+        this.random = new Random();
+    }
+    
+    public abstract void habilidadEspecial(Combatiente objetivo);
+    
+    @Override
+    public void tomarTurno(Batalla ctx) {
+        // 70% atacar, 30% habilidad especial
+        if (random.nextDouble() < 0.7) {
+            Combatiente objetivo = ctx.getJugadorAleatorio();
+            if (objetivo != null) {
+                atacarEnemigo(objetivo, ctx);
+            }
+        } else {
+            Combatiente objetivo = ctx.getJugadorAleatorio();
+            if (objetivo != null) {
+                usarHabilidadEspecial(objetivo, ctx);
+            }
+        }
+    }
+    
+    private void atacarEnemigo(Combatiente objetivo, Batalla ctx) {
+        int hpAntes = objetivo.getHp();
+        this.atacar(objetivo);
+        int daño = hpAntes - objetivo.getHp();
+        
+        String mensaje = this.getNombre() + " atacó a " + objetivo.getNombre() + 
+                        " causando " + daño + " de daño.";
+        
+        if (!objetivo.estaVivo()) {
+            mensaje += " " + objetivo.getNombre() + " ha sido derrotado!";
+        }
+        
+        ctx.registrarAccion(mensaje);
+    }
+    
+    private void usarHabilidadEspecial(Combatiente objetivo, Batalla ctx) {
+        this.habilidadEspecial(objetivo);
+        ctx.registrarAccion(this.getNombre() + " usó su habilidad especial en " + objetivo.getNombre() + ".");
+    }
+    
+    @Override
+    public void atacar(Combatiente objetivo) {
+        int daño = this.atk;
+        
+        // Aplicar bonus de ataque
+        for (EfectoTemporal efecto : this.efectos) {
+            if (efecto.getTipo() == Efecto.ATAQUE_PLUS) {
+                daño += efecto.getMagnitud();
+            }
+        }
+        
+        // Los jefes hacen más daño
+        if (esJefe) {
+            daño = (int)(daño * 1.5);
+        }
+        
+        objetivo.recibirDaño(daño);
+    }
+    
+    // Getters
+    public TipoEnemigo getTipo() { 
+        return tipo; 
+    }
+    
+    public boolean esJefe() { 
+        return esJefe; 
+    }
+    
+    @Override
+    public String toString() {
+        String titulo = esJefe ? " (Jefe)" : "";
+        return nombre + titulo + " - HP: " + hp + "/" + hpMax + " ATK: " + atk;
+    }
 }
--- Herencia/EnemigosConcretos.java
+++ Herencia/EnemigosConcretos.java
@@ -1,3 +1,163 @@
-public class EnemigosConcretos{
+class Bandido extends Enemigo {
     
+    public Bandido(boolean esJefe) {
+        super(esJefe ? "Bandido Jefe" : "Bandido", 
+              TipoEnemigo.BANDIDO, 
+              esJefe ? 80 : 50, 
+              esJefe ? 20 : 12, 
+              esJefe);
+    }
+    
+    @Override
+    public void habilidadEspecial(Combatiente objetivo) {
+        // Robo: reduce ataque del objetivo y aumenta el propio
+        int reduccion = esJefe ? 8 : 5;
+        objetivo.agregarEfecto(new EfectoTemporal(Efecto.ATAQUE_PLUS, 2, -reduccion));
+        this.agregarEfecto(new EfectoTemporal(Efecto.ATAQUE_PLUS, 2, reduccion));
+    }
+}
+
+class BandidoJefe extends Bandido {
+    
+    public BandidoJefe() {
+        super(true);
+    }
+    
+    @Override
+    public void habilidadEspecial(Combatiente objetivo) {
+        super.habilidadEspecial(objetivo); // Robo básico
+        // Habilidad extra del jefe: ataque adicional
+        this.atacar(objetivo);
+    }
+}
+
+class Chaman extends Enemigo {
+    
+    public Chaman(boolean esJefe) {
+        super(esJefe ? "Chamán Jefe" : "Chamán", 
+              TipoEnemigo.CHAMAN, 
+              esJefe ? 60 : 40, 
+              esJefe ? 15 : 10, 
+              esJefe);
+    }
+    
+    @Override
+    public void habilidadEspecial(Combatiente objetivo) {
+        // Curación
+        int cura = esJefe ? 25 : 15;
+        this.curar(cura);
+    }
+}
+
+class ChamanJefe extends Chaman {
+    
+    public ChamanJefe() {
+        super(true);
+    }
+    
+    @Override
+    public void habilidadEspecial(Combatiente objetivo) {
+        super.habilidadEspecial(objetivo); // Curación básica
+        // Habilidad extra: envenenar al objetivo
+        objetivo.agregarEfecto(new EfectoTemporal(Efecto.VENENO, 2, 3));
+    }
+}
+
+class Arquero extends Enemigo {
+    
+    public Arquero(boolean esJefe) {
+        super(esJefe ? "Arquero Jefe" : "Arquero", 
+              TipoEnemigo.ARQUERO, 
+              esJefe ? 55 : 35, 
+              esJefe ? 18 : 15, 
+              esJefe);
+    }
+    
+    @Override
+    public void habilidadEspecial(Combatiente objetivo) {
+        // Disparo preciso: daño extra
+        int daño = this.atk + (esJefe ? 15 : 8);
+        objetivo.recibirDaño(daño);
+    }
+}
+
+class ArqueroJefe extends Arquero {
+    
+    public ArqueroJefe() {
+        super(true);
+    }
+    
+    @Override
+    public void habilidadEspecial(Combatiente objetivo) {
+        super.habilidadEspecial(objetivo); // Disparo preciso
+        // Habilidad extra: reducir evasión del objetivo
+        objetivo.agregarEfecto(new EfectoTemporal(Efecto.EVASION, 2, -5));
+    }
+}
+
+class Lancero extends Enemigo {
+    
+    public Lancero(boolean esJefe) {
+        super(esJefe ? "Lancero Jefe" : "Lancero", 
+              TipoEnemigo.LANCERO, 
+              esJefe ? 70 : 45, 
+              esJefe ? 22 : 14, 
+              esJefe);
+    }
+    
+    @Override
+    public void habilidadEspecial(Combatiente objetivo) {
+        // Carga: ataque especial y acelerar
+        int daño = this.atk + (esJefe ? 10 : 6);
+        objetivo.recibirDaño(daño);
+        this.agregarEfecto(new EfectoTemporal(Efecto.ACELERACION, 1, 1));
+    }
+}
+
+class LanceroJefe extends Lancero {
+    
+    public LanceroJefe() {
+        super(true);
+    }
+    
+    @Override
+    public void habilidadEspecial(Combatiente objetivo) {
+        super.habilidadEspecial(objetivo); // Carga básica
+        // Habilidad extra: escudo
+        this.agregarEfecto(new EfectoTemporal(Efecto.ESCUDO, 2, 5));
+    }
+}
+
+class CaballeroCorompido extends Enemigo {
+    
+    public CaballeroCorompido(boolean esJefe) {
+        super(esJefe ? "Caballero Corrompido Jefe" : "Caballero Corrompido", 
+              TipoEnemigo.CABALLERO_CORROMPIDO, 
+              esJefe ? 100 : 65, 
+              esJefe ? 25 : 16, 
+              esJefe);
+    }
+    
+    @Override
+    public void habilidadEspecial(Combatiente objetivo) {
+        // Aura siniestra: daño y debuff
+        int daño = esJefe ? 12 : 8;
+        objetivo.recibirDaño(daño);
+        objetivo.agregarEfecto(new EfectoTemporal(Efecto.ATAQUE_PLUS, 3, esJefe ? -8 : -5));
+    }
+}
+
+class CaballeroCorrompidoJefe extends CaballeroCorompido {
+    
+    public CaballeroCorrompidoJefe() {
+        super(true);
+    }
+    
+    @Override
+    public void habilidadEspecial(Combatiente objetivo) {
+        super.habilidadEspecial(objetivo); // Aura siniestra
+        // Habilidades extra: fortalecerse
+        this.agregarEfecto(new EfectoTemporal(Efecto.ATAQUE_PLUS, 2, 10));
+        this.agregarEfecto(new EfectoTemporal(Efecto.ESCUDO, 2, 3));
+    }
 }
